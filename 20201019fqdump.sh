89700f4287735639a454221b6eb5edd28cd432c6
fnakagawa0101 <73159288+fnakagawa0101@users.noreply.github.com>
2020-10-27 15:04:59 +0900

    Add files via upload

---
+++ 20201019fqdump.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#fastq-dump --split-files ./SRR577595.2
+#fastq-dump --split-files ./SRR577596.2
+fastq-dump --split-files ./SRR577597.2
+fastq-dump --split-files ./SRR577598.2
+
+## 20201020SRR577597,SRR577598を処理中にストップしたので不要な部分をコメントアウトして再実行
